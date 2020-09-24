9b5dd18fa1be398a6390c8261fdde28b8e5d8098
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-09-24 20:14:20 +0900

    add iam terraform setting

--- terraform/iam/aws_iam_policy.tf
+++ terraform/iam/aws_iam_policy.tf
@@ -5,3 +5,9 @@ resource "aws_iam_policy" "deploy" {
   policy      = file("aws_iam_policies/ecr_policy.json")
 }
 
+resource "aws_iam_policy" "ecs_instance_policy" {
+  name        = "ecs-instance-policy"
+  path        = "/"
+  description = ""
+  policy      = file("aws_iam_policies/ecs_instance_policy.json")
+}
